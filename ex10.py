003243928d96952e98deb90b6cc54bba09dc0a34
goharikkh <100861779+goharikkh@users.noreply.github.com>
2022-06-22 13:56:50 +0400

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,10 @@
+import math
+a = float(input("Enter the number"))
+b = float(input("Enter the number"))
+
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a / b = ", a / b)
+print("a // b = ", a // b)
+print("a % b = ", a % b)
+print("lna = ", math.log10(a))
